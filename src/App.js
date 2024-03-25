aa23a217768023f49de9508472dcce160a365cf0
prajaktamokashi <prajaktamokashi1409@gmail.com>
2024-03-25 17:03:01 +0530

    new ajax demo programs

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
 
 import './App.css';
-import Properties from './Components/Properties';
-import Table from './Components/Table';
-import Count1 from "./Components/Counter";
-import Header from "./Components/Const";
-import FavoriteColor from './Components/Usestatedemo';
-import Timer from './Components/Useeffectdemo';
-import Component1 from './Components/Usecontextdemo';
-import Reff from './Userefdemo';
-import Todos from './Components/Usereducerdemo';
-import Callback from './Components/Usecallbackdemo';
+import Promises from './Components/Promises';
+//import Properties from './Components/Properties';
+//import Table from './Components/Table';
+//import Count1 from "./Components/Counter";
+//import Header from "./Components/Const";
+//import FavoriteColor from './Components/Usestatedemo';
+//import Timer from './Components/Useeffectdemo';
+//import Component1 from './Components/Usecontextdemo';
+//import Reff from './Userefdemo';
+//import Todos from './Components/Usereducerdemo';
+//import Callback from './Components/Usecallbackdemo';
 function App() {
   return (
     <>
-    <Header/>,
-    <Count1/>,
-    <Table/>,
-    <Properties name="Prajakta!!"/>,
-   <FavoriteColor/>,
-   <Timer/>,
-   <Component1/>,
-   <Reff/>,
-   <Todos/>,
-   <Callback/>
+<Promises/>
    </>
   );
 }
